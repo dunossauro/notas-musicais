2be295f689205017425d9233220bf052875ba34a
dunossauro <mendesxeduardo@gmail.com>
2023-01-21 01:34:52 -0300

    Implementação inicial das escalas

---
+++ notas_musicais/escalas.py
@@ -0,0 +1,32 @@
+NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
+
+
+def escalas(tonica: str, tonalidade: str) -> dict[str, list[str]]:
+    """
+    Gera uma escala apartir de uma tônica e uma tonalidade.
+
+    Args:
+        tonica: Nota que será a tônica da escala
+        tonalidade: Tonialidade da escala
+
+    Returns:
+        Um dicionário com as notas da escala e os graus.
+
+    Examples:
+        >>> escalas('C', 'maior')
+        {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+
+        >>> escalas('A', 'maior')
+        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+    """
+    intervalos = ESCALAS[tonalidade]
+    tonica_pos = NOTAS.index(tonica)
+
+    temp = []
+
+    for intervalo in intervalos:
+        nota = (tonica_pos + intervalo) % 12
+        temp.append(NOTAS[nota])
+
+    return {'notas': temp, 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
