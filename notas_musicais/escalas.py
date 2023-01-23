45253d5fd7a55cbc4dedb78850ec764cfc28c96e
dunossauro <mendesxeduardo@gmail.com>
2023-01-23 04:20:38 -0300

    Implementação da escala menor natural

--- notas_musicais/escalas.py
+++ notas_musicais/escalas.py
@@ -1,5 +1,5 @@
 NOTAS = 'C C# D D# E F F# G G# A A# B'.split()
-ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11)}
+ESCALAS = {'maior': (0, 2, 4, 5, 7, 9, 11), 'menor': (0, 2, 3, 5, 7, 8, 10)}
 
 
 def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
@@ -21,8 +21,8 @@ def escala(tonica: str, tonalidade: str) -> dict[str, list[str]]:
         >>> escala('C', 'maior')
         {'notas': ['C', 'D', 'E', 'F', 'G', 'A', 'B'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
 
-        >>> escala('a', 'maior')
-        {'notas': ['A', 'B', 'C#', 'D', 'E', 'F#', 'G#'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
+        >>> escala('a', 'menor')
+        {'notas': ['A', 'B', 'C', 'D', 'E', 'F', 'G'], 'graus': ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']}
     """
     tonica = tonica.upper()
     try:
--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -49,15 +49,18 @@ def test_deve_retornar_um_erro_dizendo_que_a_escala_não_existe():
 
 
 @mark.parametrize(
-    'tonica,esperado',
+    'tonica,tonalidade, esperado',
     [
-        ('C', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
-        ('C#', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
-        ('F', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+        ('C', 'maior', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
+        ('C#', 'maior', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
+        ('F', 'maior', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+        ('C', 'menor', ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#']),
+        ('C#', 'menor', ['C#', 'D#', 'E', 'F#', 'G#', 'A', 'B']),
+        ('F', 'menor', ['F', 'G', 'G#', 'A#', 'C', 'C#', 'D#']),
     ],
 )
-def test_deve_retornar_as_notas_corretas(tonica, esperado):
-    resultado = escala(tonica, 'maior')
+def test_deve_retornar_as_notas_corretas(tonica, tonalidade, esperado):
+    resultado = escala(tonica, tonalidade)
     assert resultado['notas'] == esperado
 
 
