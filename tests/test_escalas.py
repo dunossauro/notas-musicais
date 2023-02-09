b0ce41c9fa016e5e3d9d75887302ff131c015bee
dunossauro <mendesxeduardo@gmail.com>
2023-02-09 19:01:59 -0300

    [ERRO PARA VÍDEO] diminuindo cobertura propositalmente

--- tests/test_escalas.py
+++ tests/test_escalas.py
@@ -1,74 +1,74 @@
-"""
-AAA - 3A - A3
+# """
+# AAA - 3A - A3
 
-Arange - Act - Assets!
-Arrumar - Agir - Garantir!
-"""
-from pytest import mark, raises
+# Arange - Act - Assets!
+# Arrumar - Agir - Garantir!
+# """
+# from pytest import mark, raises
 
-from notas_musicais.escalas import ESCALAS, NOTAS, escala
-
-
-def test_deve_funcionar_com_notas_minusculas():
-    # Arrumar
-    tonica = 'c'
-    tonalidade = 'maior'
-
-    # Act - Chamo o que testar
-    result = escala(tonica, tonalidade)
-
-    # Assert
-    assert result
+# from notas_musicais.escalas import ESCALAS, NOTAS, escala
 
 
-def test_deve_retornar_um_erro_dizendo_que_a_nota_nao_existe():
-    tonica = 'X'
-    tonalidade = 'maior'
+# def test_deve_funcionar_com_notas_minusculas():
+#     # Arrumar
+#     tonica = 'c'
+#     tonalidade = 'maior'
 
-    mensagem_de_erro = f'Essa nota não existe, tente uma dessas {NOTAS}'
+#     # Act - Chamo o que testar
+#     result = escala(tonica, tonalidade)
 
-    with raises(ValueError) as error:
-        escala(tonica, tonalidade)
-
-    assert mensagem_de_erro == error.value.args[0]
-
-
-def test_deve_retornar_um_erro_dizendo_que_a_escala_não_existe():
-    tonica = 'c'
-    tonalidade = 'tonalidade'
-
-    mensagem_de_erro = (
-        'Essa escala não existe ou não foi implementada. '
-        f'Tente uma dessas {list(ESCALAS.keys())}'
-    )
-
-    with raises(KeyError) as error:
-        escala(tonica, tonalidade)
-
-    assert mensagem_de_erro == error.value.args[0]
+#     # Assert
+#     assert result
 
 
-@mark.parametrize(
-    'tonica,tonalidade, esperado',
-    [
-        ('C', 'maior', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
-        ('C#', 'maior', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
-        ('F', 'maior', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
-        ('C', 'menor', ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#']),
-        ('C#', 'menor', ['C#', 'D#', 'E', 'F#', 'G#', 'A', 'B']),
-        ('F', 'menor', ['F', 'G', 'G#', 'A#', 'C', 'C#', 'D#']),
-    ],
-)
-def test_deve_retornar_as_notas_corretas(tonica, tonalidade, esperado):
-    resultado = escala(tonica, tonalidade)
-    assert resultado['notas'] == esperado
+# def test_deve_retornar_um_erro_dizendo_que_a_nota_nao_existe():
+#     tonica = 'X'
+#     tonalidade = 'maior'
+
+#     mensagem_de_erro = f'Essa nota não existe, tente uma dessas {NOTAS}'
+
+#     with raises(ValueError) as error:
+#         escala(tonica, tonalidade)
+
+#     assert mensagem_de_erro == error.value.args[0]
 
 
-def test_deve_retornar_os_sete_graus():
-    tonica = 'c'
-    tonalidade = 'maior'
-    esperado = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+# def test_deve_retornar_um_erro_dizendo_que_a_escala_não_existe():
+#     tonica = 'c'
+#     tonalidade = 'tonalidade'
 
-    resultado = escala(tonica, tonalidade)
+#     mensagem_de_erro = (
+#         'Essa escala não existe ou não foi implementada. '
+#         f'Tente uma dessas {list(ESCALAS.keys())}'
+#     )
 
-    assert resultado['graus'] == esperado
+#     with raises(KeyError) as error:
+#         escala(tonica, tonalidade)
+
+#     assert mensagem_de_erro == error.value.args[0]
+
+
+# @mark.parametrize(
+#     'tonica,tonalidade, esperado',
+#     [
+#         ('C', 'maior', ['C', 'D', 'E', 'F', 'G', 'A', 'B']),
+#         ('C#', 'maior', ['C#', 'D#', 'F', 'F#', 'G#', 'A#', 'C']),
+#         ('F', 'maior', ['F', 'G', 'A', 'A#', 'C', 'D', 'E']),
+#         ('C', 'menor', ['C', 'D', 'D#', 'F', 'G', 'G#', 'A#']),
+#         ('C#', 'menor', ['C#', 'D#', 'E', 'F#', 'G#', 'A', 'B']),
+#         ('F', 'menor', ['F', 'G', 'G#', 'A#', 'C', 'C#', 'D#']),
+#     ],
+# )
+# def test_deve_retornar_as_notas_corretas(tonica, tonalidade, esperado):
+#     resultado = escala(tonica, tonalidade)
+#     assert resultado['notas'] == esperado
+
+
+# def test_deve_retornar_os_sete_graus():
+#     tonica = 'c'
+#     tonalidade = 'maior'
+#     esperado = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII']
+
+#     resultado = escala(tonica, tonalidade)
+
+#     assert resultado['graus'] == esperado
